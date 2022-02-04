03d78baf4b04c096529702726076a632a85668c1
MattVeall <84116475+MattVeall@users.noreply.github.com>
2022-02-04 23:58:10 +1100

    Create TCP_Server.py

---
+++ TCP_Server.py
@@ -0,0 +1,25 @@
+from http import client, server
+import imp
+import socket
+import threading
+from urllib import request
+
+IP = '0.0.0.0'
+Port = 9998
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, Port))
+    server.listen(5)
+    print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
+
+    def handle_client(client_socket):
+        with client_socket as sock:
+            request = sock.recv(1024)
+            print(f'[*] Received: {request.decode("utf-8)}')
+            sock.send(b'ACK')
+
+if __name__ == '__main':
+    main()
