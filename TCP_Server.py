fb34a62944976f895f02ba3c2c6709bca10a17da
MattVeall <84116475+MattVeall@users.noreply.github.com>
2022-02-05 00:06:45 +1100

    Update TCP_Server.py

--- TCP_Server.py
+++ TCP_Server.py
@@ -18,7 +18,7 @@ def main():
     def handle_client(client_socket):
         with client_socket as sock:
             request = sock.recv(1024)
-            print(f'[*] Received: {request.decode("utf-8)}')
+            print(f'[*] Recevied: {request.decode("utf-8")}')
             sock.send(b'ACK')
 
 if __name__ == '__main':
