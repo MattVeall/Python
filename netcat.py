37544c117bc4643cb8bdeb4e05c387f8f77e935c
MattVeall <84116475+MattVeall@users.noreply.github.com>
2022-02-07 22:34:29 +1100

    Update netcat.py

--- netcat.py
+++ netcat.py
@@ -114,7 +114,7 @@ if __name__ == '__main__':
             echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
             netcat.py -t 192.168.1.108 -p 5555 # connect to server
             '''))
-    parser.add_argument('-c', '--comand', action='store_true', help='command shell')
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command' )
     parser.add_argument('-l', '--listen', action='store_true', help='listen')
     parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
